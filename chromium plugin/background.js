6b2e08aa3724a87de4082dd5eba81b99b18aa3e0
StassDS <98816978+StassDS@users.noreply.github.com>
2024-10-14 15:02:15 +0200

    V1.3

--- chromium plugin/background.js
+++ chromium plugin/background.js
@@ -1,5 +1,5 @@
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (changeInfo.status === 'complete' && (tab.url.includes('music.youtube.com') || tab.url.includes('plaza.one') || tab.url.includes('soundcloud.com') || tab.url.includes('music.yandex.ru') || tab.url.includes('vk.com/music') || tab.url.includes('spotify.com'))) {
+    if (changeInfo.status === 'complete' && (tab.url.includes('music.youtube.com') || tab.url.includes('plaza.one') || tab.url.includes('soundcloud.com') || tab.url.includes('music.yandex.ru') || tab.url.includes('vk.com') || tab.url.includes('spotify.com'))) {
         chrome.scripting.executeScript({
             target: {tabId: tabId},
             files: ['content.js']
--- chromium plugin/content.js
+++ chromium plugin/content.js
@@ -1,6 +1,6 @@
 function getTrackInfo() {
     let artist, track, source;
-    
+
     if (window.location.hostname.includes("youtube")) {
         artist = document.querySelector(".ytmusic-player-bar .subtitle a").innerText;
         track = document.querySelector(".ytmusic-player-bar .title").innerText;
@@ -12,24 +12,19 @@ function getTrackInfo() {
     } else if (window.location.hostname.includes("soundcloud")) {
         artist = document.querySelector(".playbackSoundBadge__lightLink").innerText;
         track = document.querySelector(".playbackSoundBadge__title").innerText;
-        // Кастыль ебучий
         track = track.replace("Current track:", "").trim();
         source = "SoundCloud";
-
     } else if (window.location.hostname.includes("music.yandex.ru")) {
-        artist = document.querySelector(".d-track__artists a").innerText;
-        track = document.querySelector(".d-track__title a").innerText;
+        track = document.querySelector(".track__title").innerText;
+        artist = document.querySelector(".d-artists__expanded a").innerText;
         source = "Yandex Music";
-    } else if (window.location.hostname.includes("vk.com/music")) {
-        artist = document.querySelector(".ai_artist").innerText;
-        track = document.querySelector(".ai_title").innerText;
+    } else if (window.location.hostname.includes("vk.com")) {
+        track = document.querySelector(".top_audio_player_title").innerText;
+        artist = document.querySelector(".top_audio_player_artist").innerText;
         source = "VK Music";
     } else if (window.location.hostname.includes("spotify")) {
-        // Используем предоставленные селекторы для трека и исполнителя
         let artistElement = document.querySelector("a[data-testid='context-item-info-artist']");
         let trackElement = document.querySelector("a[data-testid='context-item-link']");
-        
-        // Проверяем наличие элементов перед присвоением значений
         if (artistElement && trackElement) {
             artist = artistElement.innerText;
             track = trackElement.innerText;
@@ -50,6 +45,7 @@ function sendTrackInfoToServer(trackInfo) {
     });
 }
 
+// Обновляем информацию каждые 500 мс
 setInterval(() => {
     const trackInfo = getTrackInfo();
     if (trackInfo) {
